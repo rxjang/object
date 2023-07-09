ab1444bc9f655e4b7b6c59983bf7c6208f69e49b
Seohyun Jang <olllehyun@gmail.com>
2023-07-09 10:31:02 +0900

    오브젝트: chap04 설계 품질과 트레이드오프

---
+++ object-book/chap04/src/main/java/ReservationAgency.java
@@ -0,0 +1,9 @@
+import java.time.chrono.ChronoLocalDateTime;
+
+public class ReservationAgency {
+
+    public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
+        Money fee = screening.calculateFee(audienceCount);
+        return new Reservation(customer, screening, fee, audienceCount);
+    }
+}
