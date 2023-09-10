d0ca78c9d16fb237eb84a213ae8008f4d2af5a12
Seohyun Jang <olllehyun@gmail.com>
2023-09-10 15:43:26 +0900

    오브젝트: chap05 책임 할당하기

---
+++ object-book/chap05/src/main/java/PeriodCondition.java
@@ -0,0 +1,26 @@
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class PeriodCondition implements DiscountCondition {
+
+    private DayOfWeek dayOfWeek;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalDateTime startTime, LocalDateTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return dayOfWeek.equals(screening.getWhenScreened().getDayOfWeek()) &&
+                startTime.compareTo(screening.getWhenScreened()) <= 0 &&
+                endTime.isAfter(screening.getWhenScreened());
+    }
+
+    @Override
+    public DiscountConditionType getType() {
+        return DiscountConditionType.PERIOD;
+    }
+}
---
+++ object-book/chap05/src/main/java/SequenceCondition.java
@@ -0,0 +1,17 @@
+public class SequenceCondition implements DiscountCondition {
+
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+
+    @Override
+    public DiscountConditionType getType() {
+        return DiscountConditionType.SEQUENCE;
+    }
+}
